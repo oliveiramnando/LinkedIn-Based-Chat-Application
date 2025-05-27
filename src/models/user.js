f55522408cc0c965a0e6d7560f9fd243942166d4
oliveiramnando <oliveiramnando@gmail.com>
2025-05-27 17:25:21 -0400

    added user schema

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mogoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    linkedinId: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    profilePicture: {
+        type: String // url to linkedin avatar
+    }
+}, { 
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
